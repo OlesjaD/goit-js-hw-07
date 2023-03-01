b8be81b76e23490077ccad5c2f820a0bc49a2c7f
OlesjaD <olesja.d.c@gmail.com>
2023-03-01 22:45:25 +0200

    markup galery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+const createGaleryItems = createGaleryEl(galleryItems);
+containerGallery.insertAdjacentHTML("beforeend", createGaleryItems);
+
+containerGallery.addEventListener('click', onGalleryClick);
+
+function createGaleryEl(galleryItems) {
+    return galleryItems 
+        .map(({preview, original, description}) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>
+            `;
+        }).join("");
+};
+
+function onGalleryClick (event) {
+    event.preventDefault();
+    console.log(event.target.nodeName);
+    console.log(event.target.dataset.source);
+};
